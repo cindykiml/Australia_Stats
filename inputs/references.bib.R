3d81aa251539f6ad4af404602f80927db5dfaf2d
Eden Barker <edenbarker@Edens-MacBook-Air.local>
2022-02-26 23:42:29 -0500

    commit by eden

---
+++ inputs/references.bib.R
@@ -0,0 +1,79 @@
+@Manual{R,
+  title = {R: A Language and Environment for Statistical Computing},
+  author = {{R Core Team}},
+  organization = {R Foundation for Statistical Computing},
+  address = {Vienna, Austria},
+  year = {2020},
+  url = {https://www.R-project.org/},
+}
+
+@Manual{Dplyr,
+  title = {dplyr: A Grammar of Data Manipulation},
+  author = {Hadley Wickham and Romain François and Lionel Henry and Kirill Müller},
+  year = {2021},
+  note = {R package version 1.0.7},
+  url = {https://CRAN.R-project.org/package=dplyr},
+}
+
+@Manual{Janitor,
+  title = {janitor: Simple Tools for Examining and Cleaning Dirty Data},
+  author = {Sam Firke},
+  year = {2021},
+  note = {R package version 2.1.0},
+  url = {https://CRAN.R-project.org/package=janitor},
+}
+
+@Manual{Tidyr,
+  title = {tidyr: Tidy Messy Data},
+  author = {Hadley Wickham and Maximilian Girlich},
+  year = {2022},
+  note = {R package version 1.2.0},
+  url = {https://CRAN.R-project.org/package=tidyr},
+}
+
+@Article{Tidyverse,
+  title = {Welcome to the {tidyverse}},
+  author = {Hadley Wickham and Mara Averick and Jennifer Bryan and Winston Chang and Lucy DAgostino McGowan and Romain François and Garrett Grolemund and Alex Hayes and Lionel Henry and Jim Hester and Max Kuhn and Thomas Lin Pedersen and Evan Miller and Stephan Milton Bache and Kirill Müller and Jeroen Ooms and David Robinson and Dana Paige Seidel and Vitalie Spinu and Kohske Takahashi and Davis Vaughan and Claus Wilke and Kara Woo and Hiroaki Yutani},
+    year = {2019},
+    journal = {Journal of Open Source Software},
+    volume = {4},
+    number = {43},
+    pages = {1686},
+    doi = {10.21105/joss.01686},
+}
+  
+@Article{Tidyverse,
+    title = {A Tsunami of Disability Is Coming as a Result of ‘Long COVID’},
+    year = {2021},
+    author = {Claire Pomeroy},
+    url = {https://www.scientificamerican.com/article/a-tsunami-of-disability-is-coming-as-a-result-of-lsquo-long-covid-rsquo/},
+ }
+
+@Publication{AusGov_SocialServices,
+    title = {Disability Royal Commission Support Services fact sheet - Easy Read version},
+    year = {2020},
+    author = {Department of Social Services, Australian Government},
+    url = {https://www.dss.gov.au/sites/default/files/documents/07_2021/easy-readdrc-do-you-need-support.pdf},
+}
+
+@Publication{AusGov_DisabilityServices,
+  title = {National Standards for Disability Services},
+  year = {2013},
+  author = {Department of Social Services, Australian Government},
+  url = {https://www.dss.gov.au/our-responsibilities/disability-and-carers/standards-and-quality-assurance/national-standards-for-disability-services},
+}
+
+@Publication{AusGov_MentalHealthServices,
+  title = {National Standards for Disability Services},
+  year = {2013},
+  author = {Department of Social Services, Australian Government},
+  url = {https://www.dss.gov.au/our-responsibilities/disability-and-carers/standards-and-quality-assurance/national-standards-for-disability-services},
+}
+
+@Publication{AusGov_VolunteerServices,
+  title = {Volunteer Management Activity FAQs},
+  year = {2021},
+  author = {Department of Social Services, Australian Government},
+  url = {https://www.dss.gov.au/sites/default/files/documents/10_2021/vma-faqs-dss-website-update-october-2021.pdf},
+}
+  
